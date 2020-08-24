ca5e91ad758187b807b88eaf03395aacf84bd283
rohitgoyal9293 <goyal.rohit9293@gmail.com>
2020-08-25 03:54:57 +0530

    Updates

---
+++ precache-manifest.66f4cd70fa43330febdfbada48cfc314.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10097a08e3c044f0402d0d5da666284",
+    "url": "/Bluestack-Frontend-Dev-Challenge/index.html"
+  },
+  {
+    "revision": "38c4db5d0d5bd11a45ac",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/css/2.f3e39853.chunk.css"
+  },
+  {
+    "revision": "e3061a661e444c5a2a54",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/css/main.e069427a.chunk.css"
+  },
+  {
+    "revision": "38c4db5d0d5bd11a45ac",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/js/2.d1495826.chunk.js"
+  },
+  {
+    "revision": "f6f897265d57399127478b1973bf92d3",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/js/2.d1495826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3061a661e444c5a2a54",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/js/main.9e71f348.chunk.js"
+  },
+  {
+    "revision": "70d605141b8a633ebe6d",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/js/runtime-main.f8c9b838.js"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/media/Bitmap-1.977ecf7b.png"
+  },
+  {
+    "revision": "7173cfe7840a5f2d3853b4220f6cf851",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/media/Bitmap-3.7173cfe7.png"
+  },
+  {
+    "revision": "90640a8778af0217304f5effb1aa8413",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/media/Bitmap-4.90640a87.png"
+  },
+  {
+    "revision": "e51f42c349763884e809677c9634b288",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/media/Bitmap-5.e51f42c3.png"
+  },
+  {
+    "revision": "c161ba4abb12e86093bc396fd2337b9d",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/media/Bitmap-6.c161ba4a.png"
+  },
+  {
+    "revision": "8a4d68dbcc8a535baeeef974e67aaed2",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/media/Bitmap-7.8a4d68db.png"
+  },
+  {
+    "revision": "3ecfc5bd32f7be1a974631a1e152d0b3",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/media/Bitmap-8.3ecfc5bd.png"
+  },
+  {
+    "revision": "1f4b57666ba11e9abc2cec5cb76828ff",
+    "url": "/Bluestack-Frontend-Dev-Challenge/static/media/logo.1f4b5766.png"
+  }
+]);
